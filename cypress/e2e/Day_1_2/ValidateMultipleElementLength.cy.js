1744577762e76ffc7bd398c4495c5976d28846e1
LakshmisriAkula <akulalakshmisri@gmail.com>
2025-07-04 12:54:52 +0530

    changes

--- cypress/e2e/Day_1_2/ValidateMultipleElementLength.cy.js
+++ cypress/e2e/Day_1_2/ValidateMultipleElementLength.cy.js
@@ -1,10 +1,21 @@
-describe("Validate element length",()=>{
-    it("validate length of t-shirt", function(){
+describe("Validate element length", () => {
+
+       // Ignore uncaught exceptions from the site
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false;
+    });
+    
+    it("validate length of t-shirt", function () {
         cy.visit("https://automationexercise.com/")
         cy.get("[href='#Men']").click();
         cy.get("[href='/category_products/3']").click();
-        cy.get(".features_items .col-sm-4").should('have.length',6)
+        cy.get(".features_items .col-sm-4").should('have.length', 6)
 
+        cy.get(".features_items .col-sm-4").eq(0).contains("Add to cart").click();
+
+        cy.wait(5000);
+
+        cy.get(".btn.btn-success").click();
 
     })
 })
